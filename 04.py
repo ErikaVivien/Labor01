30ede931e533022cab73a49f874dbbc8c98623d7
adamc7651546213 <yolol7116@gmail.com>
2023-09-28 10:10:37 +0200

    4.

--- 04.py
+++ 04.py
@@ -2,23 +2,27 @@
 
 
 
-def adatkeres():
-    szam = input("Kérem az első számot: ")
-    while not szam.isnumeric():
-        print("Rossz érték!")
-        szam = input("Kérem az első számot: ")
-    szam = int(szam)
+def adatkeres(tipus):
+    szam = ""
+    if tipus == "sz":
+        szam = input("Kérem a számot: ")
+        while not szam.isnumeric():
+            print("Rossz érték!")
+            szam = input("Kérem a számot: ")
+        szam = int(szam)
+    elif tipus == "n":
+        pass
     return szam
 
 print("Számológép")
-szam1 = adatkeres()
+szam1 = adatkeres("sz")
 
-muvelet = input("Milyen művelet legyen (+, -, *, /)?: ")
+muvelet = input("Milyen művelet legyen (+, -, *, / )?: ")
 while muvelet not in {"+", "-", "*", "/"}:
        print("Rossz műveleti jel!")
-       muvelet = input("Milyen művelet legyen (+, -, *, /)?: ")
+       muvelet = input("Milyen művelet legyen (+, -, *, / )?: ")
 
-szam2 = adatkeres()
+szam2 = adatkeres("sz")
 
 eredmeny =0
 if muvelet == "+":
@@ -33,5 +37,5 @@ elif muvelet == "/":
 print(str(szam1).rjust(50))
 print(muvelet, end="")
 print(str(szam2).rjust(49))
-print("".rjust(50),"_")
+print("".rjust(50),"")
 print(str(eredmeny).rjust(50))
