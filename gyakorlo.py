0c91b2a577e56d54987ca41215b6a726ae6c6ba7
ErikaVivien <146320919+ErikaVivien@users.noreply.github.com>
2023-10-27 16:47:40 +0200

    Add files via upload

---
+++ gyakorlo.py
@@ -0,0 +1,31 @@
+# print("Hello","how are you?")
+# print("Hello","how are you?", sep="-----")
+# print("Hello","\nhow are you?")
+
+# valaki = input("Hogy hívnak?  ")
+# print(f'Üdv,\n{valaki}\na fedélzeten!!!\n')
+
+# szam1 = int(input("Kérek egy számot: "))
+# szam2 = int(input("Kérek még egy számot: "))
+
+# print("Az egyik szám: ", szam1)
+# print("A másik szám: ", szam2)
+
+# eredmeny = szam1 + szam2
+# print("A {0} és a {1} összege:".format(szam1,szam2), eredmeny)
+
+# kupon = input("Van kuponod? ")
+# if kupon == "i" or kupon == "igen":
+              # print("Ingyenes a belépés")
+# else:
+    # print("Fizetned kell")
+    # kor = int(input("Mennyi idős vagy? "))
+    # if kor <= 8:
+        # print("100 Ft a jegy")
+    # elif kor <= 18:
+        # print("1000 Ft a jegy")
+#     elif kor <= 65:
+        # print("2000 Ft a jegy")
+#     else:
+        # print("100 Ft a jegy")
+
